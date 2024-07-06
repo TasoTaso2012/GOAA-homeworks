2adc8aa8964f557f3cbf13264ee282c17109f8df
TasoTaso2012 <adolfscars777@gmail.com>
2024-07-06 15:46:21 +0400

    lesson 04

---
+++ day 04/homework/homework1.py
@@ -0,0 +1,11 @@
+age=12+34
+print(age)
+
+number=47-23
+print(number)
+
+age=5*4
+print(age)
+
+number=23/7
+print(number)
---
+++ day 04/homework/homework3.py
@@ -0,0 +1,7 @@
+user_name=("anastasia")
+
+birth_date=("22 september 2012")
+
+print(user_name)
+print(birth_date)
+
