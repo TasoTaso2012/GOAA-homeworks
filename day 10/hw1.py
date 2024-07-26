25b441b8d65b088ff11c4e8020551c03a4ab5032
TasoTaso2012 <adolfscars777@gmail.com>
2024-07-26 19:39:19 +0400

    lesson 10

---
+++ day 10/hw1.py
@@ -0,0 +1,24 @@
+5>3 and 3>5 #false
+9<2 and 2<9 #false
+5<=2 and 7<=4 #false
+10>=8 and 9>=4 #true
+2==2 and 5==2 #false
+7==1 and 4==4 #false
+5!=5 and 3!=3 #false
+2!=8 and 5!=3 #true
+4>2 and 9>4 #true
+7<9 and 5<8 #true
+
+6>4 or 4>6 #true
+10<2 or 3<9 #true
+6<=2 or 7<=4 #false
+10>=8 or 10>=5 #true
+3==3 or 6==3 #true
+8==2 or 5==5 #true
+6!=6 or 4!=4 #false
+3!=9 or 6!=4 #true
+5>3 or 10>5 #true
+7<10 or 6<9 #true
+
+
+
