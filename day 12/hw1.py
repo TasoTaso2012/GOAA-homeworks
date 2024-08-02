ffacd3d72052829072d38b9fb4133e46ed846f4a
TasoTaso2012 <adolfscars777@gmail.com>
2024-08-02 22:02:56 +0400

    lesson 12

---
+++ day 12/hw1.py
@@ -0,0 +1,3 @@
+#in python,sequencing is arrangament of things in specific order/პითონში თანმიმდევრობა არის საგნების განლაგება კონკრეტულ თანმიმდევრობით.
+#iteration is the execution of the same code over and over again/ეს არის იგივე კოდის განმეორებით შესრულება.
+#selection is making a choice if its true or not/ეს არის არჩევანის გაკეთება,მართალია თუ არა.
---
+++ day 12/hw2.py
@@ -0,0 +1,9 @@
+user_age=int(input())
+
+father_age=37
+
+father_age_after_23years=60
+user_age_after_23years=35
+
+print(int(father_age_after_23years - user_age_after_23years))
+
