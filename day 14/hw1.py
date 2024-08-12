74944b30bfc2a5bed3f5a87e51c246c37fb3ac67
TasoTaso2012 <adolfscars777@gmail.com>
2024-08-12 17:03:18 +0400

    lesson 14

---
+++ day 14/hw1.py
@@ -0,0 +1,13 @@
+for i in range(1):
+    print(i)
+    print(1)
+    print(2)
+    print(3)
+    print(4)
+    print(5)
+    print(6)
+    print(7)
+    print(8)
+    print(9)
+    print(10)
+
---
+++ day 14/hw4.py
@@ -0,0 +1,4 @@
+Name=input()
+
+for i in range(5):
+    print("hello "+Name)
