a7020d46ba8a5733802b10e3fb3028d052cfc874
TasoTaso2012 <adolfscars777@gmail.com>
2024-08-15 02:24:32 +0400

    lesson 15

---
+++ day 15/homework/hw2.py
@@ -0,0 +1,49 @@
+#all the things i've leaned in python!
+
+#variables
+name="anastasia"
+
+age=12
+
+print(age)
+print(name)
+
+#data types
+print(type(name))
+print(type(age))
+
+#arithmetic operacions
+age1=10
+age2=20
+
+print(age1+age2)
+print(age2-age1)
+print(age1*age2)
+print(age2/age1)
+
+#for loop
+for i in range(5):
+    print("Goa is the best academy")
+
+#comparison operations
+#true
+print(2<4)
+print(10>4)
+print(10<=10)
+print(4>=2)
+#false
+print(4>6)
+print(3<1)
+print(3<=1)
+print(8>=9)
+
+#input and output
+user_name=input()
+print("hello "+ user_name)
+
+
+
+
+
+
+
