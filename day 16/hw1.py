0ea01f2bf94b12bc3cf0e965dff7fd3d054df769
TasoTaso2012 <adolfscars777@gmail.com>
2024-08-17 15:13:24 +0400

    lesson 16

--- day 16/hw1.py
+++ day 16/hw1.py
@@ -1,1 +1,35 @@
-name=input('enter your name :')
+age=1
+while age <= 5:
+    print(age)
+    age = age+1
+
+
+
+number=1
+while number <= 10 :
+    print("hello")
+    number = number+1
+
+
+
+secret_number=7
+guess=int(input("guess the secret number between 1 and 10: "))
+while guess != secret_number:
+    guess=int(input("try again:"))
+print("you guessed right!")
+
+
+
+number=0
+while number <= 2:
+    print("goa is the best!")
+    number=number+1
+
+
+
+
+secret_number=3
+guess=int(input("guess the secret number between 1 and 10: "))
+while guess != secret_number:
+    guess=int(input("try again:"))
+print("you guessed right!")
