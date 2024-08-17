eea0aa6fccfc1fa9a344bc07cea60575e4479107
TasoTaso2012 <adolfscars777@gmail.com>
2024-08-17 14:21:28 +0400

    lesson 14

--- day 14/hw1.py
+++ day 14/hw1.py
@@ -1,13 +1,4 @@
-for i in range(1):
+for i in range(1,10):
     print(i)
-    print(1)
-    print(2)
-    print(3)
-    print(4)
-    print(5)
-    print(6)
-    print(7)
-    print(8)
-    print(9)
-    print(10)
+  
 
--- day 14/hw6.py
+++ day 14/hw6.py
@@ -1,17 +1,3 @@
-for i in range(1):
+for i in range(1,15):
     print(i*2)
-    print(1*2)
-    print(2*2)
-    print(3*2)
-    print(4*2)
-    print(5*2)
-    print(6*2)
-    print(7*2)
-    print(8*2)
-    print(9*2)
-    print(10*2)
-    print(11*2)
-    print(12*2)
-    print(13*2)
-    print(14*2)
-    print(15*2)
+    
