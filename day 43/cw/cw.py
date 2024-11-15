9bfba674fe3f3b4ea47e4456ede6b7da7cae4339
TasoTaso2012 <adolfscars777@gmail.com>
2024-11-15 16:26:23 +0400

    lesson 43

---
+++ day 43/cw/cw.py
@@ -0,0 +1,20 @@
+#1 
+for i in range(5, 15):
+
+    print(i)
+
+#2
+for i in range(5, 15, 2):
+    print(i)
+
+#3
+for i  in range(5, 15):
+
+    print(i + 1)
+
+#4
+#  It picks values from a list,
+#  It gives each value to a variable.
+#  The code inside the loop runs for each value of i.
+
+
---
+++ day 43/hw/hws.py
@@ -0,0 +1,34 @@
+#1
+num1 = [1, 2, 3, 4, 5]
+print(sum(num1))  
+
+#2
+num2 = [1, 2, 3, 4, 5]
+print(max(num2))  
+
+#3
+num3 = [1, 2, 3, 4, 5]
+print(min(num3))
+
+#4
+num4 = [1, 2, 3, 4, 5]
+print(len(num4))  
+
+#5
+strings = ["mango", "banana", "apple", "strawberry"]
+strings.append("orange") 
+print(strings)
+
+
+#6
+
+words = ["mango", "orange", "watermelon", "cherry"]
+words.insert(2,"banana") 
+print(words)
+
+#7
+
+words = ["mango", "orange", "watermelon", "cherry"]
+words.pop(2) 
+print(words)
+
