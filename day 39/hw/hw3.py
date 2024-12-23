06d1a427e9cf06e0499d32fd38bac652d081b89e
TasoTaso2012 <adolfscars777@gmail.com>
2024-12-23 21:15:39 +0400

    lesson 39

--- day 39/hw/hw3.py
+++ day 39/hw/hw3.py
@@ -1,6 +1,6 @@
-list4 = [1, 2, 3, 4, 5]
-list5 = [6, 7, 8, 9, 10]
-list6 = [11 , 12 , 13 , 14 , 15]
+list5 = [1, 2, 3, 4, 5]
+list6 = [6, 7, 8, 9, 10]
+list7 = [11 , 12 , 13 , 14 , 15]
 
 print("Lengths of the lists:")
-print(len(list4), len(list5), len(list6))
+print(len(list5), len(list6), len(list7))
